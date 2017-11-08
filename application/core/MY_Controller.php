6d2fb2e832ca867f79c63c16df616f3a989b18b6
Rezi Apriliansyah <Reziapriliansyah@gmail.com>
2017-11-08 10:22:19 +0700

    adding Ionic Template

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -18,8 +18,10 @@ class MY_Controller extends CI_Controller
         // Access-Control headers are received during OPTIONS requests
         if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS')
         {
-            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) 
+            	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) 
+            	header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
             exit(0);
         }
 	}
