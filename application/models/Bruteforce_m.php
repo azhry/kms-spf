45f2b0bd95dbc4b81150a62128306ad908d118ab
azhry <arliansyah_azhary@yahoo.com>
2018-04-18 10:59:59 +0700

    revisi pencarian

--- application/models/Bruteforce_m.php
+++ application/models/Bruteforce_m.php
@@ -18,7 +18,7 @@ class Bruteforce_m extends MY_Model {
 		$result = [];
 		foreach ( $data as $row ) {
 
-			$str = strtolower( $row->kinerja );
+			$str = strtolower( $row->judul );
 			$s_len = strlen( $str );
 			if ( $q_len > $s_len ) continue;
 
@@ -26,7 +26,7 @@ class Bruteforce_m extends MY_Model {
 
 			if ( strtolower( $query ) == 'baik'  ) {
 
-				if ( strtolower( $query ) == strtolower( $row->kinerja ) ) {
+				if ( strtolower( $query ) == strtolower( $row->judul ) ) {
 
 					$result []= $row;
 
@@ -44,7 +44,7 @@ class Bruteforce_m extends MY_Model {
 							if ( $i + $j >= $s_len ) break;
 
 							if ( $q_arr[$j] != $s_arr[$i + $j] ) {
-								
+
 								$found = false;
 								break;
 							
@@ -92,15 +92,15 @@ class Bruteforce_m extends MY_Model {
 	public function search_knowledge( $query ) {
 
 		$this->load->model( 'explicit_knowledge_m' );
-		$this->load->model( 'tacit_knowledge_m' );
+		// $this->load->model( 'tacit_knowledge_m' );
 
 		$knowledge = $this->explicit_knowledge_m->get_knowledge();
-		$tacit = $this->tacit_knowledge_m->get_knowledge();
-		foreach ( $tacit as $row ) {
+		// $tacit = $this->tacit_knowledge_m->get_knowledge();
+		// foreach ( $tacit as $row ) {
 
-			$knowledge []= $row;
+		// 	$knowledge []= $row;
 
-		}
+		// }
 
 		return $this->search( $query, $knowledge );
 
